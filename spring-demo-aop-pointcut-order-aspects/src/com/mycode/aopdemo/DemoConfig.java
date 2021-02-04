11595b58616445c4729ed4690c60b311d309d435
Shahriat Hossain <somel90@gmail.com>
2021-02-04 08:23:28 +0600

    AOP: Ordering Aspects

---
+++ spring-demo-aop-pointcut-order-aspects/src/com/mycode/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.mycode.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.mycode.aopdemo")
+public class DemoConfig {
+
+}
